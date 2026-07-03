528ed3c6242ef35af126d12269d4aafea06d3c5f
rlatapy-luna <remi.latapy@lunabee.com>
2026-07-03 10:42:25 +0200

    Add modifier param to DefaultBlockingContent

--- buildSrc/src/main/kotlin/AndroidConfig.kt
+++ buildSrc/src/main/kotlin/AndroidConfig.kt
@@ -73,7 +73,7 @@ object AndroidConfig {
     const val EXTENSIONS_VERSION: String = "4.11.0"
     const val TEST_VERSION: String = "4.9.0"
     const val KTOR_VERSION: String = "4.10.0"
-    const val LOADING_VERSION: String = "4.10.0"
+    const val LOADING_VERSION: String = "4.10.1"
     const val PLATFORM_VERSION: String = "4.14.0"
     const val EXTENSIONS_ANDROID_VERSION: String = "4.9.0"
     const val MONITORING_CORE_VERSION: String = "4.9.1"
